ee8e20b2761334675d79a430e841edeb60cd7f72
alt <pch957@163.com>
2015-05-22 10:48:17 +0800

    forgot transactions

--- btsbots_wallet/metadata.py
+++ btsbots_wallet/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'btsbots-wallet'
 project = "web wallet for BitShares"
 project_no_spaces = project.replace(' ', '')
-version = '0.1.5'
+version = '0.1.6'
 description = 'web wallet for BitShares'
 authors = ['Alt']
 authors_string = ', '.join(authors)
